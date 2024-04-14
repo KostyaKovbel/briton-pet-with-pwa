92641d5b15800de0768d3bee22de70939bbd3ecd
breadbq <k.kovbel@do-it.co>
2024-04-14 17:18:11 +0300

    squash

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
+  reloadOnOnline: true,
 });
 
 const nextConfig = {
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head></Head>
+      <Head />
+
       <link rel="manifest" href="/manifest.json" />
       <title>Наявність плівки | Briton</title>
       <link rel="icon" href="/logo.png" />
